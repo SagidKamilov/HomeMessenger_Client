fd6fc983774f4b4a05c4ba1932116594ccfc73e0
SagidKamilov <89615854074a@gmail.com>
2024-04-24 10:39:02 +0300

    initial

---
+++ app/src/main/java/com/example/chatapp/activities/SignUpActivity.java
@@ -0,0 +1,36 @@
+package com.example.chatapp.activities;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.chatapp.R;
+import com.example.chatapp.databinding.ActivitySignUpBinding;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    private ActivitySignUpBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setListeners();
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+    }
+
+    private void setListeners() {
+        binding.textSignIn.setOnClickListener(v -> getOnBackPressedDispatcher().onBackPressed());
+    }
+
+}
