daa46f9d4a92bc94b68561b4bbabde3cc40c61a6
SagidKamilov <89615854074a@gmail.com>
2024-04-26 19:26:31 +0300

    Основное окно

---
+++ app/src/main/java/com/example/chatapp/activities/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.chatapp.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.chatapp.R;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.);
+    }
+}
